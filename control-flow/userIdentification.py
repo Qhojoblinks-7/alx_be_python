584db3869109f31b27c1c0d8b03e15ef07c9f451
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-07-21 16:41:39 +0000

    fix: changed the input variable to 'weather' forclarity and consistency.     type: refactor     scope: control-flow     description: Updated the variable name to improve readability and maintainability.     issue: N/A

--- control-flow/userIdentification.py
+++ control-flow/userIdentification.py
@@ -1,21 +1,21 @@
 # Identifying auth user
 
-# logged_in = True  # Try changing this to False later
-# is_admin = False  # Try changing this to True later
+logged_in = True  # Try changing this to False later
+is_admin = False  # Try changing this to True later
 
-# if logged_in and is_admin:
-#     print("Show Admin Panel Link")
-# else:
-#     print("Access Denied")
+if logged_in and is_admin:
+    print("Show Admin Panel Link")
+else:
+    print("Access Denied")
 
 
-# is_admin = False
-# is_premium_subscriber = False
+is_admin = False
+is_premium_subscriber = False
 
-# if is_admin == True or is_premium_subscriber == True:
-#     print("Access granted")
-# else:
-#     print("Access Denied")
+if is_admin == True or is_premium_subscriber == True:
+    print("Access granted")
+else:
+    print("Access Denied")
 
 
 for num in range(1,6):
--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,16 +1,18 @@
 # The following code snippet demonstrates a simple weather recommendation system using if-elif-else statements.
 # It takes user input for the weather condition and provides appropriate clothing suggestions.# This code snippet is a simple calculator using match-case statements to perform basic arithmetic operations.
 
-weatherInput = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
 
-if weatherInput == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weatherInput == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weatherInput == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
     
-    # commit messages
-    # feat: Added a weather recommendation system based on user input
+    
+    # fix: changed the input variable to 'weather' for 
+    # ref: 
+    # feat:
